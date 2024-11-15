e220d0e1ea04c9f6dd98b4e507e671099a292474
a37547-duc <75719437+a37547-duc@users.noreply.github.com>
2024-11-15 21:32:45 +0700

    Add files via upload

--- passports/passport.local.js
+++ passports/passport.local.js
@@ -36,12 +36,40 @@ const loginRegisterService = require("../service/loginRegisterService");
 const { v4: uuidv4 } = require("uuid");
 uuidv4();
 
+// module.exports = new LocalStrategy(
+//   {
+//     usernameField: "email",
+//     passwordField: "password",
+//   },
+//   async (req, email, password, done) => {
+//     try {
+//       const user = await loginRegisterService.handleUserLogin({
+//         email,
+//         password,
+//       });
+
+//       if (!user) {
+//         console.log("Không tìm thấy người dùng!");
+//         return done(null, false, {
+//           message: "Email hoặc mật khẩu không đúng.",
+//           err: "email",
+//         });
+//       }
+//       console.log(user);
+//       return done(null, user);
+//     } catch (err) {
+//       console.error("Đã xảy ra lỗi khi đăng nhập:", err);
+//       return done(err);
+//     }
+//   }
+// );
+
 module.exports = new LocalStrategy(
   {
     usernameField: "email",
     passwordField: "password",
   },
-  async (req, email, password, done) => {
+  async (email, password, done) => {
     try {
       const user = await loginRegisterService.handleUserLogin({
         email,
@@ -52,7 +80,6 @@ module.exports = new LocalStrategy(
         console.log("Không tìm thấy người dùng!");
         return done(null, false, {
           message: "Email hoặc mật khẩu không đúng.",
-          err: "email",
         });
       }
       console.log(user);
