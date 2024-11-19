2f9ba4b0cc79dff635dc52673c10c0ea4044eed0
a37547-duc <75719437+a37547-duc@users.noreply.github.com>
2024-11-19 11:58:33 +0700

    Add files via upload

--- config/admin.firebase.js
+++ config/admin.firebase.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("D:/A37547-ChuyendeTN/bai1/config/chuyendetn-43b4d-firebase-adminsdk-n6bg5-457fec611b.json");
+const serviceAccount = require("./chuyendetn-43b4d-firebase-adminsdk-n6bg5-457fec611b.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
