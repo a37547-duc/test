b59462595bc72fe8fcc956aa3c0f549bcafabbaf
a37547-duc <75719437+a37547-duc@users.noreply.github.com>
2024-12-12 09:47:39 +0700

    Add files via upload

--- controllers/admin/admin.orders.controller.js
+++ controllers/admin/admin.orders.controller.js
@@ -27,27 +27,7 @@ const getAdminOrders = async (req, res) => {
   }
 };
 
-const updateAdminOrder = async (req, res) => {
-  const { id } = req.params;
-  const { orderStatus } = req.body;
-
-  try {
-    const order = await Order.findByIdAndUpdate(
-      id,
-      { orderStatus },
-      {
-        new: true,
-      }
-    );
-    if (!order) {
-      res.status(404).json({ message: "Không tìm thấy đơn hàng" });
-    }
-    res.status(200).json({ message: "Thông tin đơn hàng đã cập nhật", order });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
+// const updateAdminOrder = async (req, res) => {
 //   const { id } = req.params;
 //   const { orderStatus } = req.body;
 
@@ -66,7 +46,29 @@ const updateAdminOrder = async (req, res) => {
 //   } catch (error) {
 //     res.status(500).json({ message: error.message });
 //   }
-// });
+// };
+
+const updateAdminOrder = async (req, res) => {
+  const { id } = req.params;
+  const { orderStatus } = req.body;
+
+  try {
+    const update = { orderStatus };
+    if (orderStatus === "Giao hàng thành công") {
+      update.paymentStatus = "Đã thanh toán";
+    }
+
+    const order = await Order.findByIdAndUpdate(id, update, {
+      new: true,
+    });
+    if (!order) {
+      res.status(404).json({ message: "Không tìm thấy đơn hàng" });
+    }
+    res.status(200).json({ message: "Thông tin đơn hàng đã cập nhật", order });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 module.exports = {
   getAdminOrders,
